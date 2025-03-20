1f4459caa00692df82db22075817f385e22831ec
Atom-gnomov <gorbach0864213579@gmail.com>
2025-03-20 22:37:43 +0200

    complete

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,4 +1,3 @@
-from multiprocessing.resource_tracker import register
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
--- taxi/models.py
+++ taxi/models.py
@@ -9,18 +9,30 @@ class Manufacturer(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = "manufacturer"
+        verbose_name_plural = "manufacturers"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name = "Driver"
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    manufacturer = models.ForeignKey(
+        Manufacturer, on_delete=models.CASCADE, related_name="cars"
+    )
     drivers = models.ManyToManyField(Driver)
 
     def __str__(self) -> str:
         return self.model
+
+    class Meta:
+        verbose_name = "car"
+        verbose_name_plural = "cars"
+        verbose_name = "drivers"
