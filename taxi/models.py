276b9aea30bd7b7f2f4ad192c310c7e8fe4787a7
Atom-gnomov <gorbach0864213579@gmail.com>
2025-03-20 21:58:23 +0200

    complete

---
+++ taxi/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Driver"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField(Driver)
+
+    def __str__(self) -> str:
+        return self.model
